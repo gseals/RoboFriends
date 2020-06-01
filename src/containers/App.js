bc6726495621dd34651adb9dde155708a7742c22
Gabriel Seals <sealsgj@gmail.com>
2020-06-01 14:32:29 -0500

    trying to get load to work correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
+// import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -34,9 +34,9 @@ class App extends Component {
       <h1 className='f1'>RoboFriends</h1>
       <SearchBox searchChange={this.onSearchChange}/>
       <Scroll>
-        <ErrorBoundary>
+        {/* <ErrorBoundary> */}
         <CardList robots={filteredRobots} />
-        </ErrorBoundary>
+        {/* </ErrorBoundary> */}
       </Scroll>
       </div>
       );
